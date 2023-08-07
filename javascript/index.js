539ab3386778189629fae1f2975a43b26a632ccb
julianaflima <juli.faccio@gmail.com>
2023-08-07 08:43:31 +0530

    Add function to extract only necessary information from json

--- javascript/index.js
+++ javascript/index.js
@@ -1,15 +1,5 @@
-async function getWeatherInfo() {
-	try {
-		const response = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=34a457572d684d08a2114501230308&q=london-city-of-london-greater-london-united-kingdom&days=3&aqi=yes&alerts=yes`, {mode: "cors"});
-		const data = await response.json();
-		console.log(data);
+import { CityData, getWeatherData } from "./api.js";
 
-		// const currentTemp = data.
-		
-	} catch(e) {
-		// statements
-		console.log(e);
-	}
-}
+let cityName = 'london-city-of-london-greater-london-united-kingdom';
 
-getWeatherInfo();
+getWeatherData(cityName);
