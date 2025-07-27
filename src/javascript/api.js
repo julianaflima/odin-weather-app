c24afff0b9928a29f7f5a20cecc541a63d006372
Juliana Lima <juli.faccio@gmail.com>
2025-07-27 12:00:19 +0530

    catch errors when fetch gives an invalid result

--- src/javascript/api.js
+++ src/javascript/api.js
@@ -1,5 +1,6 @@
 import { metricToUs } from "./temp_unit.js"
 const API_KEY = 'ZYMNF6E2WY66VDTXW4B5Y33MD';
+const API_KEY_GEOCODE = '68833fc17bbae270338907cmtf7545f';
 
 class CityData {
 	constructor() {
@@ -14,42 +15,60 @@ class CityData {
 
 async function getWeatherData(cityName) {
 	try {
+
+		// This API would fix the problem with not loading weather if the input is just 'beng'.
+		// const testResponse = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=34a457572d684d08a2114501230308&q=${cityName}`, {mode: "cors"});
+		// const testData = await testResponse.json();
+		// console.log(testData);
+
+
 		const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${cityName}?unitGroup=metric&key=${API_KEY}`, {mode: "cors"});
-		const data = await response.json();
-		// console.log(data);
 
-		const city = new CityData;
+		// If statement to catch invalid response data
+		if (response.ok) {
+			const data = await response.json();
 
-		// Add current weather information to Object
-		city.location = data.resolvedAddress;
-		city.current.condition_text = data.currentConditions.conditions;
-		city.current.icon = data.currentConditions.icon;
-		city.current.temp = {};
-		city.current.tempMax = {};
-		city.current.tempLow = {};
-		city.current.temp.metric = Math.round(data.days[0].temp);
-		city.current.temp.us = metricToUs(city.current.temp.metric)
-		city.current.tempMax.metric = Math.round(data.days[0].tempmax);
-		city.current.tempMax.us = metricToUs(city.current.tempMax.metric);
-		city.current.tempLow.metric = Math.round(data.days[0].tempmin);
-		city.current.tempLow.us = metricToUs(city.current.tempLow.metric);
-		city.current.chanceRain = Math.round(data.days[0].precipprob);
-		city.current.humidity = Math.round(data.days[0].humidity);
+			const city = new CityData;
 
-		// // Location
-		// city.location.name = data.location.name;
-		// city.location.country = data.location.country;
-		// city.location.time_zone = data.location.tz_id;
-
-		//TODO Alert
+			// Add current weather information to Object
+			city.current.condition_text = data.currentConditions.conditions;
+			city.current.icon = data.currentConditions.icon;
+			city.current.temp = {};
+			city.current.tempMax = {};
+			city.current.tempLow = {};
+			city.current.temp.metric = Math.round(data.days[0].temp);
+			city.current.temp.us = metricToUs(city.current.temp.metric)
+			city.current.tempMax.metric = Math.round(data.days[0].tempmax);
+			city.current.tempMax.us = metricToUs(city.current.tempMax.metric);
+			city.current.tempLow.metric = Math.round(data.days[0].tempmin);
+			city.current.tempLow.us = metricToUs(city.current.tempLow.metric);
+			city.current.chanceRain = Math.round(data.days[0].precipprob);
+			city.current.humidity = Math.round(data.days[0].humidity);
 
 
-		// TEST FUNCTION FOR FORECAST
-		nextXDaysForecast(city, data, 3);
-		
+			// Get city, state, country from latitude and longitude
+			const latitude = data.latitude;
+			const longitude = data.longitude;
+			const responseGeocode = await fetch(`https://geocode.maps.co/reverse?lat=${latitude}&lon=${longitude}&api_key=${API_KEY_GEOCODE}`, {mode: "cors"});
+			const cityCountryFromLatLon = await responseGeocode.json();
+			city.location.city = cityCountryFromLatLon.address.city;
+			city.location.state = getState(cityCountryFromLatLon.address.state);
+			city.location.country = cityCountryFromLatLon.address.country;
 
-		// console.log(city);
-		return city;
+			//TODO Alert
+
+
+			// TEST FUNCTION FOR FORECAST
+			nextXDaysForecast(city, data, 3);
+
+			// console.log(city);
+
+
+			return city;
+		} else {
+			// In case the response is not a valid JASON object
+			return false
+		}
 		
 	} catch(e) {
 		// statements
@@ -60,7 +79,7 @@ async function getWeatherData(cityName) {
 async function getAutocompleteOptions(inputName) {
 	const response = await fetch(`http://api.weatherapi.com/v1/search.json?key=${API_KEY}&q=${inputName}`, {mode: "cors"});
 	const data = await response.json();
-	// console.log(data);
+	console.log(data);
 	return data
 }
 
@@ -96,6 +115,18 @@ function nextXDaysForecast(city, data, numberOfDays) {
 		}
 	
 		city.forecast.push(followingDay);
+	}
+}
+
+
+function getState(state) {
+	if (state) {
+		state = state + ', '
+		return state;
+	}
+	else {
+		state = '';
+		return state
 	}
 }
 
