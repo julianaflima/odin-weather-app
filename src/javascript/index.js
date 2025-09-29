d54986ffdd5f1946efcd84fc3c8d5814400daaa0
Juliana Lima <juli.faccio@gmail.com>
2025-09-29 17:22:01 +0530

    change default city to test workflow

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -59,4 +59,4 @@ displayContainer.appendChild(container);
 
 // Default city
 const selectedUnit = tempUnit(toggleUnit.checked);
-showCityWeather("Curitiba", selectedUnit);
+showCityWeather("Athens", selectedUnit);
